0807e9a9c2480ff977e5bce3e07bab81c3f7d464
Catminusminus <getomya@svk.jp>
2024-11-10 00:21:40 +0900

    ファイルの分割

---
+++ 9cc.h
@@ -0,0 +1,75 @@
+#ifndef __NINE_CC_H__
+#define __NINE_CC_H__
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+Node *expr();
+
+void codegen(Node *node);
+
+// 入力プログラム
+extern char *user_input;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...);
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...);
+
+Token *tokenize();
+Node *expr();
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+
+#endif 
